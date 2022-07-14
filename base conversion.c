00a5a4c80f8054d223987dfd8ec01616d67035c7
Ramzy Hisham <96325470+ramzyhisham@users.noreply.github.com>
2022-07-14 16:04:29 +0530

    Add files via upload

---
+++ base conversion.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+	int b,n,i,r,digit,p,count=0;
+	char a[100];
+	printf("Enter the number: ");
+	scanf("%d",&n);
+	printf("\nEnter the base to be converted: ");
+	scanf("%d",&b);
+	p=n;
+	do
+	{
+		r=p%b;
+		digit='0'+r;
+		if(digit>'9')
+		  digit=digit+7;
+		a[count]=digit;
+		count++;
+		p=p/b;
+	} while(p!=0);
+	printf("\nBase %d equivalent of num %d is: ",b,n);
+	for(i=count-1;i>=0;--i)
+	printf("%c",a[i]);
+	printf(".\n");
+}
